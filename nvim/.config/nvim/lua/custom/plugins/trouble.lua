013d3d04fb487a031315be08129ca0691316e526
Kaendor <dimteboul@gmail.com>
2024-05-29 08:28:29 +0200

    add git trouble for diagnostics

---
+++ nvim/.config/nvim/lua/custom/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  'folke/trouble.nvim',
+  branch = 'dev',
+  opts = {},
+  keys = {
+    {
+      '<leader>dw',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = '[D]iagnostics [W]orkspace',
+    },
+  },
+}

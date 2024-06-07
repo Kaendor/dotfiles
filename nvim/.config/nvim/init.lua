a4ec5ef25732513e8e8ac5ab3bee26b519746f5f
Kaendor <dimteboul@gmail.com>
2024-06-07 13:02:44 +0200

    remove winbar and statusbar in oil and trouble

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,87 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -232,7 +148,7 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_floating_shadow = false
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.o.guifont = 'FiraCode Nerd Font:h14'
+  vim.o.guifont = 'FiraCode Nerd Font:h13'
 end
 
 -- [[ Install `lazy.nvim` plugin manager ]]
@@ -1055,13 +971,16 @@ require('lazy').setup({
     },
     opts = {
       extensions = { 'oil', 'trouble', 'lazy', 'toggleterm' },
-      disabled_filetypes = {
-        winbar = {
-          'oil',
-          'trouble',
-        },
-        statusline = {
-          'oil',
+      options = {
+        disabled_filetypes = {
+          winbar = {
+            'oil',
+            'trouble',
+          },
+          statusline = {
+            'oil',
+            'trouble',
+          },
         },
       },
       sections = {
@@ -1101,7 +1020,17 @@ require('lazy').setup({
       },
       -- TODO: disable winbar in oil buffer
       winbar = {
-        lualine_a = { 'filename' },
+        lualine_a = {
+          function()
+            local ok, oil = pcall(require, 'oil')
+            if ok then
+              return vim.fn.fnamemodify(oil.get_current_dir(), ':~')
+            else
+              return ''
+            end
+          end,
+          'filename',
+        },
         lualine_b = { "require'nvim-navic'.get_location()" },
         -- lualine_z = {},
       },

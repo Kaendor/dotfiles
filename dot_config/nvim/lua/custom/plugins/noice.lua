3105e9d4a185d5ac14100d5c57f5798141746e35
Dimitri Teboul <dimitri.teboul@indy.fr>
2025-07-25 12:10:30 +0200

    feat(breadcrumb): remove lualine breadcrumb and try dropbar

--- dot_config/nvim/lua/custom/plugins/noice.lua
+++ dot_config/nvim/lua/custom/plugins/noice.lua
@@ -1,26 +1,27 @@
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  opts = {
-    cmdline = {
-      view = 'cmdline',
-    },
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-      },
-    },
-    presets = {
-      inc_rename = true,
-      command_palette = true,
-      bottom_search = true,
-      lsp_doc_border = true,
-    },
-  },
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-  },
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		cmdline = {
+			view = "cmdline",
+		},
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+		presets = {
+			inc_rename = true,
+			command_palette = true,
+			bottom_search = true,
+			lsp_doc_border = true,
+			long_message_to_split = true,
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
 }
